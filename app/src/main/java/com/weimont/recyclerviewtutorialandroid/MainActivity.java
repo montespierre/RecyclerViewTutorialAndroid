abd50bfe625a9fcefe423400200eca7fbcd7ce40
unknown <montespierre@gmail.com>
2021-04-22 06:26:27 -0500

    first commit

---
+++ app/src/main/java/com/weimont/recyclerviewtutorialandroid/MainActivity.java
@@ -0,0 +1,38 @@
+package com.weimont.recyclerviewtutorialandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Tutorial
+    // https://www.youtube.com/watch?v=fnavhYGqZD4
+
+    ArrayList<String> listDatos;
+    RecyclerView recycler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recycler = findViewById(R.id.recyclerIdMain);
+        recycler.setLayoutManager(new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false));
+
+        listDatos = new ArrayList<String>();
+
+        for(int i = 0; i <50; i++ ){
+            listDatos.add("Dato # " + i + " ");
+        }
+
+        AdapterDatos adapter = new AdapterDatos(listDatos);
+        recycler.setAdapter(adapter);
+
+    }
+}
